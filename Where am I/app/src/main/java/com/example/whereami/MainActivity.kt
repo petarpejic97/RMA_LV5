108755a0931342a3844b4c3586bebe4238b9af23
petarpejic97 <petar.pejic@outlook.com>
2020-05-15 17:34:34 +0200

    napravljene informacije o adresi i dodana karta

--- Where am I/app/src/main/java/com/example/whereami/MainActivity.kt
+++ Where am I/app/src/main/java/com/example/whereami/MainActivity.kt
@@ -1,12 +1,113 @@
 package com.example.whereami
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.pm.PackageManager
+import android.location.*
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import android.location.Geocoder
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+import java.util.*
 
-class MainActivity : AppCompatActivity() {
 
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private val locationPermission = Manifest.permission.ACCESS_FINE_LOCATION
+    private val locationRequestCode = 10
+    private lateinit var locationManager: LocationManager
+    private lateinit var geocoder: Geocoder
+    private lateinit var map: GoogleMap
+
+    private val locationListener = object: LocationListener {
+        override fun onProviderEnabled(provider: String?) { }
+        override fun onProviderDisabled(provider: String?) { }
+        override fun onStatusChanged(provider: String?, status: Int, extras: Bundle?) { }
+        override fun onLocationChanged(location: Location?) {
+            updateLocationDisplay(location)
+        }
+    }
+    private fun updateLocationDisplay(location: Location?) {
+
+        val lat = location!!.latitude
+        val lon = location.longitude
+
+        mygeosirina.text = "$lat"
+        mygeoduzina.text = "$lon"
+
+        setAddress(lat,lon)
+
+    }
+    @SuppressLint("SetTextI18n")
+    private fun setAddress(lat : Double, lon : Double){
+        geocoder = Geocoder(baseContext, Locale.getDefault())
+        val adress = geocoder.getFromLocation(lat,lon,1)
+        mydrzava.text = adress[0].countryName
+        mymjesto.text = adress[0].locality
+        myadresa.text =adress[0].thoroughfare+ " " +adress[0].subThoroughfare;
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        trackLocationAction.setOnClickListener{ trackLocation() }
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
     }
+    override fun onMapReady(googleMap: GoogleMap) {
+        map = googleMap
+        val osijek = LatLng(45.55111, 18.69389)
+        map.addMarker(MarkerOptions().position(osijek).title("Marker in Osijek"))
+        map.mapType = GoogleMap.MAP_TYPE_SATELLITE
+        map.uiSettings.isZoomControlsEnabled = true
+        map.moveCamera(CameraUpdateFactory.newLatLng(osijek))
+    }
+    private fun trackLocation() {
+        if(hasPermissionCompat(locationPermission)){
+            startTrackingLocation()
+        } else {
+            requestPermisionCompat(arrayOf(locationPermission), locationRequestCode)
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray) {
+        when(requestCode){
+            locationRequestCode -> {
+                if(grantResults.size == 1 && grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                    trackLocation()
+                else
+                    Toast.makeText(this, "permision denied", Toast.LENGTH_SHORT).show()
+            }
+            else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        }
+    }
+    private fun startTrackingLocation() {
+        Log.d("TAG", "Tracking location")
+        val criteria: Criteria = Criteria()
+        criteria.accuracy = Criteria.ACCURACY_FINE
+        val provider = locationManager.getBestProvider(criteria, true)
+        val minTime = 1000L
+        val minDistance = 10.0F
+        try{
+            locationManager.requestLocationUpdates(provider, minTime, minDistance, locationListener)
+        } catch (e: SecurityException){
+            Toast.makeText(this, "permision denied", Toast.LENGTH_SHORT).show()
+        }
+    }
+    override fun onPause() {
+        super.onPause()
+        locationManager.removeUpdates(locationListener)
+    }
+
 }
---
+++ Where am I/app/src/main/java/com/example/whereami/PermissionUtils.kt
@@ -0,0 +1,15 @@
+package com.example.whereami
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+fun AppCompatActivity.hasPermissionCompat (permission: String): Boolean{
+    return ActivityCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED
+}
+fun AppCompatActivity.shouldShowRationaleCompat(permission: String): Boolean{
+    return ActivityCompat.shouldShowRequestPermissionRationale(this, permission)
+}
+fun AppCompatActivity.requestPermisionCompat(permission: Array<String>, requestCode: Int){
+    ActivityCompat.requestPermissions(this, permission, requestCode)
+}
